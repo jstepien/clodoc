507f9f180ddd901af9a0ea1c1a9a7d803f24e992
Jan Stępień <jstepien@users.sourceforge.net>
2010-09-29 00:26:47 +0200

    Updated some dependencies

--- project.clj
+++ project.clj
@@ -2,9 +2,9 @@
   :description "The description"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [compojure "0.4.0"]
+                 [compojure "0.4.1"]
                  [hiccup "0.2.6"]
-                 [ring/ring-servlet "0.2.3"]
+                 [ring/ring-servlet "0.2.6"]
                  [net.sf.jsr107cache/jsr107cache "1.1"]]
   :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.3"]
                      [ring/ring-jetty-adapter "0.2.3"]]
