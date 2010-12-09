88897f6db35061a9605dc7b8487f2b0149f53f9f
Jan Stępień <jstepien@users.sourceforge.net>
2010-12-09 22:54:48 +0100

    Added App Engine's libraries to dependencies

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
                  [compojure "0.4.1"]
                  [hiccup "0.2.6"]
                  [ring/ring-servlet "0.2.6"]
-                 [net.sf.jsr107cache/jsr107cache "1.1"]]
+                 [net.sf.jsr107cache/jsr107cache "1.1"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.4.0"]
+                 [com.google.appengine/appengine-jsr107cache "1.4.0"]]
   :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.3"]
                      [ring/ring-jetty-adapter "0.2.3"]]
   :namespaces [docjure.core])
