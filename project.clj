1405e42e52ce002a8ac25f14aba7d26786b09945
Jan Stępień <jstepien@users.sourceforge.net>
2011-09-01 10:51:35 +0200

    Updated dependencies, Clojure downgraded to 1.2.1

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject docjure "0.0.1-SNAPSHOT"
   :description "The description"
-  :dependencies [[org.clojure/clojure "1.3.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [compojure "0.5.3"]
-                 [hiccup "0.3.1"]
-                 [ring/ring-servlet "0.3.5"]
+                 [compojure "0.6.5"]
+                 [hiccup "0.3.6"]
+                 [ring/ring-servlet "0.3.11"]
                  [net.sf.jsr107cache/jsr107cache "1.1"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.4.0"]
-                 [com.google.appengine/appengine-jsr107cache "1.4.0"]]
-  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.11"]
-                     [ring/ring-jetty-adapter "0.3.5"]]
-  :namespaces [docjure.core])
+                 [com.google.appengine/appengine-api-1.0-sdk "1.4.3"]
+                 [com.google.appengine/appengine-jsr107cache "1.4.3"]]
+  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.13"]
+                     [ring/ring-jetty-adapter "0.3.11"]]
+  :aot [docjure.core])
