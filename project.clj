25bba035d07b84143633dfd47a6279536610e4b7
Jan Stępień <jstepien@users.sourceforge.net>
2011-09-02 00:25:22 +0200

    Removed leiningen-war from project.clj

--- project.clj
+++ project.clj
@@ -8,6 +8,5 @@
                  [net.sf.jsr107cache/jsr107cache "1.1"]
                  [com.google.appengine/appengine-api-1.0-sdk "1.4.3"]
                  [com.google.appengine/appengine-jsr107cache "1.4.3"]]
-  :dev-dependencies [[uk.org.alienscience/leiningen-war "0.0.13"]
-                     [ring/ring-jetty-adapter "0.3.11"]]
+  :dev-dependencies [[ring/ring-jetty-adapter "0.3.11"]]
   :aot [docjure.core])
