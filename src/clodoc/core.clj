f5850a6a2b1da60510ea7094465adf7882a633d3
Jan Stępień <jstepien@users.sourceforge.net>
2011-09-26 12:55:58 +0200

    Add Google Analytics' script

--- src/clodoc/core.clj
+++ src/clodoc/core.clj
@@ -85,6 +85,20 @@
                 (form/text-field "what")
                 (form/submit-button "Search")))
 
+(defn google-analytics
+  []
+  (javascript-tag
+   (js/compress
+     "var _gaq = _gaq || [];
+     _gaq.push(['_setAccount', 'UA-1018917-12']);
+     _gaq.push(['_trackPageview']);
+
+     (function() {
+     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+     })();")))
+
 (defn layout
   [title-coll & body]
   (html
@@ -103,7 +117,8 @@
        "body, pre, code { background: #1B2426; color: silver;"
        "font-family: \"Inconsolata\", monospace; font-size: 16px;"
        "width: 960px; margin: 10px auto;}"
-       "a { color: #FFAA3E; }"]]
+       "a { color: #FFAA3E; }"]
+      (google-analytics)]
      [:body
       [:h1 (apply title-with-links title-coll)]
       search-form
